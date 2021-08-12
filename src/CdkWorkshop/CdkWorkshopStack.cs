51562d4c44e6b3af1cb5b73b8861abe2d461ad9d
root <root@W105MN4GB3.brk.emea.dell.com>
2021-08-12 15:41:45 +0100

    add pieline

--- src/CdkWorkshop/CdkWorkshopStack.cs
+++ src/CdkWorkshop/CdkWorkshopStack.cs
@@ -1,19 +1,23 @@
 using Amazon.CDK;
-using Amazon.CDK.AWS.Lambda;
 using Amazon.CDK.AWS.APIGateway;
+using Amazon.CDK.AWS.Lambda;
 using Eladb.DynamoTableViewer;
 
 namespace CdkWorkshop
 {
     public class CdkWorkshopStack : Stack
     {
-        internal CdkWorkshopStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        public readonly CfnOutput HCViewerUrl;
+        public readonly CfnOutput HCEndpoint;
+
+        // Defines a new lambda resource
+        public CdkWorkshopStack(Construct parent, string id, IStackProps props = null) : base(parent, id, props)
         {
             var hello = new Function(this, "HelloHandler", new FunctionProps
             {
-                Runtime = Runtime.NODEJS_14_X, // execution environment
-                Code = Code.FromAsset("lambda"), // Code loaded from the "lambda" directory
-                Handler = "hello.handler" // file is "hello", function is "handler"
+                Runtime = Runtime.NODEJS_14_X,
+                Code = Code.FromAsset("lambda"),
+                Handler = "hello.handler"
             });
 
             var helloWithCounter = new HitCounter(this, "HelloHitCounter", new HitCounterProps
@@ -21,17 +25,25 @@ namespace CdkWorkshop
                 Downstream = hello
             });
 
-            // defines an API Gateway REST API resource backed by our "hello" function.
-            new LambdaRestApi(this, "Endpoint", new LambdaRestApiProps
+            var gateway = new LambdaRestApi(this, "Endpoint", new LambdaRestApiProps
             {
                 Handler = helloWithCounter.Handler
             });
 
-            // Defines a new TableViewer resource
-            new TableViewer(this, "ViewerHitCount", new TableViewerProps
+            var tv = new TableViewer(this, "ViewerHitCount", new TableViewerProps
             {
                 Title = "Hello Hits",
                 Table = helloWithCounter.MyTable
+            });
+
+            this.HCViewerUrl = new CfnOutput(this, "TableViewerUrl", new CfnOutputProps
+            {
+                Value = gateway.Url
+            });
+
+            this.HCEndpoint = new CfnOutput(this, "GatewayUrl", new CfnOutputProps
+            {
+                Value = gateway.Url
             });
         }
     }
