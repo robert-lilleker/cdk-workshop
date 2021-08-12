32f8f791593dec472a6b748854722f3476c7d1b9
root <root@W105MN4GB3.brk.emea.dell.com>
2021-08-12 13:41:55 +0100

    add pieline

--- src/CdkWorkshop/PipelineStack.cs
+++ src/CdkWorkshop/PipelineStack.cs
@@ -1,5 +1,8 @@
 using Amazon.CDK;
 using Amazon.CDK.AWS.CodeCommit;
+using Amazon.CDK.AWS.CodePipeline;
+using Amazon.CDK.AWS.CodePipeline.Actions;
+using Amazon.CDK.Pipelines;
 
 namespace CdkWorkshop
 {
@@ -11,6 +14,42 @@ namespace CdkWorkshop
             var repo = new Repository(this, "WorkshopRepo", new RepositoryProps
             {
                 RepositoryName = "WorkshopRepo"
+            });
+
+            // Defines the artifact representing the sourcecode
+            var sourceArtifact = new Artifact_();
+            // Defines the artifact representing the cloud assembly
+            // (cloudformation template + all other assets)
+            var cloudAssemblyArtifact = new Artifact_();
+
+            // The basic pipeline declaration. This sets the initial structure
+            // of our pipeline
+            var pipeline = new CdkPipeline(this, "Pipeline", new CdkPipelineProps
+            {
+                PipelineName = "WorkshopPipeline",
+                CloudAssemblyArtifact = cloudAssemblyArtifact,
+
+                // Generates the source artifact from the repo we created in the last step
+                SourceAction = new CodeCommitSourceAction(new CodeCommitSourceActionProps
+                {
+                    ActionName = "CodeCommit", // Any Git-based source control
+                    Output = sourceArtifact, // Indicates where the artifact is stored
+                    Repository = repo // Designates the repo to draw code from
+                }),
+
+                // Builds our source code outlined above into a could assembly artifact
+                SynthAction = SimpleSynthAction.StandardNpmSynth(new StandardNpmSynthOptions
+                {
+                    SourceArtifact = sourceArtifact,  // Where to get source code to build
+                    CloudAssemblyArtifact = cloudAssemblyArtifact,  // Where to place built source
+
+                    InstallCommands = new []
+                    {
+                        "npm install -g aws-cdk"
+                    },
+                    BuildCommands = new [] { "dotnet build src" }, // Language-specific build cmd
+                    SynthCommand = "cdk synth"
+                })
             });
         }
     }
